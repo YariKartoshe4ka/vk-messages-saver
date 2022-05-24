7c1f9c01f357f2b0127f8525745b5577838fde49
YariKartoshe4ka <yaroslav.kikel.06@inbox.ru>
2022-05-24 17:57:09 +0300

    Документация saver.py, улучшена обработка reply_message

--- vkms/saver.py
+++ vkms/saver.py
@@ -1,33 +1,80 @@
 from os import makedirs
 
 
+def save(out_dir, peer_id, fmt, msgs):
+    """Конвертирует и сохраняет сообщения в удобном для чтения формате
+
+    Args:
+        out_dir (str): Абсолютный путь к каталогу, в котором находится
+            результат работы программы
+        peer_id (int): Идентификатор переписки, сообщения которой
+            необходимо сохранить
+        fmt (str): Формат, в котором нужно сохранить переписку
+        msgs (List[Message]): Список всех сообщений переписки
+    """
+    globals()['save_' + fmt](out_dir, peer_id, msgs)
+
+
+def save_txt(out_dir, peer_id, msgs):
+    """Сохраняет переписку в формате TXT. Идея взята у hikiko4ern
+    Ссылка: [https://github.com/hikiko4ern/vk_dump]
+    """
+    # Создаем папку для хранения переписки
+    makedirs(f'{out_dir}/dialogs/txt/', exist_ok=True)
+
+    # Сохраняем конвертированный текст
+    with open(f'{out_dir}/dialogs/txt/{peer_id}.txt', 'w') as file:
+        file.write(convert_txt(msgs))
+
+
 def convert_txt(msgs):
+    # Буффер текста переписки
     buf = ''
+
     prev_msg = None
 
     for msg in msgs:
+        # Если предыдущего сообщения не было, пишем дату текущего сообщения
         if prev_msg is None:
             buf += f'        [{msg.full_date()}]\n'
 
+        # Если предыдущее и текущее сообщения были отправлены в разные дни,
+        # пишем дату текущего сообщения
         elif msg.date.day - prev_msg.date.day >= 1:
             buf += f'\n        [{msg.full_date()}]\n'
             prev_msg = None
 
+        # Буффер текста текущего сообщения (каждый элемент - новая строка)
         text = []
 
+        # Если сообщение является сервисным, добавляем текст действия
         if msg.action:
             text.append(f'[{msg.action}]')
 
+        # Если сообщение отправлено в ответ на другое, обрабатываем другое
+        # сообщение отдельно
         if msg.reply_msg:
-            for line in filter(None, convert_txt([msg.reply_msg]).split('\n')):
-                text.append('> ' + line)
+            convert_txt([msg.reply_msg])
 
+            for line in msg.reply_msg.text_parsed:
+                text.append('{username} {line}'.format(
+                    username=(
+                        msg.reply_msg.username + '>'
+                        if line == msg.reply_msg.text_parsed[0] else
+                        ' ' * (len(msg.reply_msg.username) + 1)
+                    ),
+                    line=line
+                ))
+
+        # Добавляем текст самого сообщения (если есть)
         if msg.text:
-            text.append(msg.text)
+            text.extend(msg.text.split('\n'))
 
+        # Если есть пересланные сообщения, обрабатываем их рекурсивно
         for line in filter(None, convert_txt(msg.fwd_msgs).split('\n')):
             text.append('| ' + line)
 
+        # Обрабатываем медиавложения
         for atch in msg.atchs:
             if atch.tp == 'photo':
                 text.append(f'[фото: {atch.hash}]')
@@ -38,6 +85,10 @@ def convert_txt(msgs):
             else:
                 text.append('{uknown attachment}')
 
+        # Кэшируем полученный текст
+        msg.text_parsed = text
+
+        # Записываем полученный текст в буффер переписки
         for line in text:
             if line == text[0]:
                 if prev_msg and prev_msg.username == msg.username:
@@ -46,19 +97,11 @@ def convert_txt(msgs):
                     username = msg.username + ':'
 
                 buf += f"[{msg.time()}] {username} {line}\n"
-
             else:
                 buf += f"{' ' * 7} {' ' * len(msg.username)}  {line}\n"
 
+        # Обновляем предыдущее сообщение
         prev_msg = msg
 
+    # Возвращаем буффер
     return buf
-
-
-def save(out_dir, peer_id, fmt, msgs):
-    makedirs(f'{out_dir}/{fmt}/', exist_ok=True)
-
-    converter = globals()['convert_' + fmt]
-
-    with open(f'{out_dir}/{fmt}/{peer_id}.{fmt}', 'w') as file:
-        file.write(converter(msgs))
